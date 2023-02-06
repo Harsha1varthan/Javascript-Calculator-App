fd3c0bfa5be8202f37518d150614cfc3f2929a65
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2023-02-06 01:22:36 +0100

    Added the Theme Auto-detection Functionality

--- js/togglebtn.js
+++ js/togglebtn.js
@@ -1,17 +1,21 @@
 var checkbox = document.querySelector('input[name=theme]');
 
-let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition')
-    }, 500)
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  document.documentElement.setAttribute("data-theme", "dark");
+  checkbox.checked = true;
+} else {
+  document.documentElement.setAttribute("data-theme", "light");
+  checkbox.checked = false;
 }
+
 checkbox.addEventListener('change', function() {
     if(this.checked) {
-        trans()
+        document.documentElement.classList.add("transition");
         document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.theme = "dark";
     } else {
-        trans()
+        document.documentElement.classList.add("transition");
         document.documentElement.setAttribute('data-theme', 'light')
+        localStorage.theme = "light";
     }
 })
